ebcdf11145774b08f0900639cb51a06e000b19d1
Nicholas Calleja <nick.calleja545@gmail.com>
2020-03-29 18:21:47 -0400

    I've finished the EnemyClass for now, it will be updated later but for now it should have everything we need

--- Dasher/Assets/_Scripts/EnemyClass.cs
+++ Dasher/Assets/_Scripts/EnemyClass.cs
@@ -16,6 +16,9 @@ public class EnemyClass : MonoBehaviour
     // This is directing the path for our Enemy AI
     Waypoint wPoints;
 
+    // Waypoint Index
+    int wpIndex;
+
     // The Player Target
     // This is the direction of the player
     public Transform target;
@@ -53,11 +56,50 @@ public class EnemyClass : MonoBehaviour
     // Attack Method   
     public void Attack(Transform player)
     {
-
+        // ...
     }    
 
+    // If the Player approaches an Enemy, the Enemy will Attack
+    private void OnTriggerEnter2D(Collider2D coll) 
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            float tempSpd = spd;
+            spd = 0;
+            Attack(target);
+            spd = tempSpd;
+        }
+    }
 
     */
 
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves the Enemy to the next waypoint depending on it's speed and position
+            // Vector2 is a 2D vector from point to point
+        transform.position = Vector2.MoveTowards(transform.position, wPoints.waypoints[wpIndex].position, spd * Time.deltaTime);  
+        
+        // If the Enemy is within .1 of the waypoint then switch to the next waypoint (or back to the first one)
+        if(Vector2.Distance(transform.position, wPoints.waypoints[wpIndex].position) < 0.1f)
+        {
+            // If it's not the last waypoint then continue
+            if(wpIndex < wPoints.waypoints.Length - 1)
+            {
+                // Move to the next waypoint
+                wpIndex++;
+            }
+            // Else, switch back to the last one
+            // UPDATE: This will be changed to manage going from multiple different waypoints
+            else
+            {
+                // Setting the next destination to be back to the beginning
+                    // UPDATE: if this is buggy try setting it to 1
+                wpIndex = 0;
+            }
+        }
 
+        // Calling to check if the Enemy is dead
+        Death();
+    }
 }
