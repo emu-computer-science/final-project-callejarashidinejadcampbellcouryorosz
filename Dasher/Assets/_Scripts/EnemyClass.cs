793577cd964c4a664803179bb53df744768c086a
Nicholas Calleja <nick.calleja545@gmail.com>
2020-04-05 23:43:43 -0400

    The demo isn't finished, but the scene is created with a lot of bugs

--- Dasher/Assets/_Scripts/EnemyClass.cs
+++ Dasher/Assets/_Scripts/EnemyClass.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // This is the Basic Enemy Class
-<<<<<<< HEAD
-    // We're looking to apply this to multiple different types of enemies
+// We're looking to apply this to multiple different types of enemies
 public class EnemyClass : MonoBehaviour
 {
 
@@ -19,35 +18,22 @@ public class EnemyClass : MonoBehaviour
         Dying
     }
 
-=======
-// We're looking to apply this to multiple different types of enemies
-public class EnemyClass : MonoBehaviour
-{
-
->>>>>>> PlayerMovement
     // Basic Attributes
     public float hp;
     public float dmg;
     public float spd;
-<<<<<<< HEAD
     public float attSpd;
     public bool isRanged;
 
     // Waypoint Array Object
-        // This is directing the path for our Enemy AI
-=======
-
-    // Waypoint Array Object
     // This is directing the path for our Enemy AI
->>>>>>> PlayerMovement
     Waypoint wPoints;
 
     // Waypoint Index
     int wpIndex;
 
     // The Player Target
-<<<<<<< HEAD
-        // This is the direction of the player
+    // This is the direction of the player
     GameObject target;
 
     // The State of the Enemy
@@ -55,15 +41,10 @@ public class EnemyClass : MonoBehaviour
 
     // Is the Enemy Attacking?
     bool isAttack;
-=======
-    // This is the direction of the player
-    public Transform target;
->>>>>>> PlayerMovement
 
     // Start Method
     void Start()
     {
-<<<<<<< HEAD
         // Starting State is Idle
         state = State.Idle;
 
@@ -73,17 +54,14 @@ public class EnemyClass : MonoBehaviour
         // No Target Yet
         target = null;
 
-=======
->>>>>>> PlayerMovement
         // Creating the Waypoints by looking at GameObjects with the tag "Waypoints"
         wPoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoint>();
     }
 
-<<<<<<< HEAD
     // Update is called once per frame
     void Update()
     {
-       switch (state)
+        switch (state)
         {
             case State.Idle:
                 Idle();
@@ -107,7 +85,7 @@ public class EnemyClass : MonoBehaviour
         Debug.Log("Current state is: " + state.ToString());
 
         // Moves the Enemy to the next waypoint depending on it's speed and position
-            // Vector2 is a 2D vector from point to point
+        // Vector2 is a 2D vector from point to point
         transform.position = Vector2.MoveTowards(transform.position, wPoints.waypoints[wpIndex].position, spd * Time.deltaTime);
 
         // If the Enemy is within .1 of the waypoint then switch to the next waypoint (or back to the first one)
@@ -115,63 +93,6 @@ public class EnemyClass : MonoBehaviour
         {
             // If it's not the last waypoint then continue
             if (wpIndex < wPoints.waypoints.Length - 1)
-=======
-    // Method to Take Damage
-    public void takeDamage(int damage)
-    {
-        // Subtract damage from Enemies Health
-        hp = hp - damage;
-    }
-
-    // Method to confirm Death and remove the Enemy Object
-    public void Death()
-    {
-
-        // If the Enemies hp reaches 0.0
-        if (hp == 0.0f)
-        {
-            // Destroy the Enemy GameObject
-            Destroy(gameObject);
-        }
-    }
-
-    /*
-    
-     // UPDATE: This can be finished after the PlayerClass has been implemented
-
-    // Attack Method   
-    public void Attack(Transform player)
-    {
-        // ...
-    }    
-
-    // If the Player approaches an Enemy, the Enemy will Attack
-    private void OnTriggerEnter2D(Collider2D coll) 
-    {
-        if(collision.gameObject.CompareTag("Player"))
-        {
-            float tempSpd = spd;
-            spd = 0;
-            Attack(target);
-            spd = tempSpd;
-        }
-    }
-
-    */
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Moves the Enemy to the next waypoint depending on it's speed and position
-            // Vector2 is a 2D vector from point to point
-        transform.position = Vector2.MoveTowards(transform.position, wPoints.waypoints[wpIndex].position, spd * Time.deltaTime);  
-        
-        // If the Enemy is within .1 of the waypoint then switch to the next waypoint (or back to the first one)
-        if(Vector2.Distance(transform.position, wPoints.waypoints[wpIndex].position) < 0.1f)
-        {
-            // If it's not the last waypoint then continue
-            if(wpIndex < wPoints.waypoints.Length - 1)
->>>>>>> PlayerMovement
             {
                 // Move to the next waypoint
                 wpIndex++;
@@ -181,7 +102,6 @@ public class EnemyClass : MonoBehaviour
             else
             {
                 // Setting the next destination to be back to the beginning
-<<<<<<< HEAD
                 wpIndex = 0;
             }
         }
@@ -194,7 +114,7 @@ public class EnemyClass : MonoBehaviour
         hp = hp - damage;
 
         // If HP drops below 0
-        if(hp <= 0.0f)
+        if (hp <= 0.0f)
         {
             // Change State to Dying
             state = State.Dying;
@@ -220,7 +140,7 @@ public class EnemyClass : MonoBehaviour
 
     // If a Player approaches an Enemy, the Enemy will attack
     private void OnTriggerEnter2D(Collider2D collision)
-    {   
+    {
         // See if the gameobject has the Player Tag
         if (collision.gameObject.CompareTag("Player"))
         {
@@ -235,7 +155,7 @@ public class EnemyClass : MonoBehaviour
         Debug.Log("Current state is: " + state.ToString());
 
         // If you can target the player
-        if(player != null)
+        if (player != null)
         {
 
             // Go toward your target to attack
@@ -248,7 +168,7 @@ public class EnemyClass : MonoBehaviour
                 TestDummy d = player.GetComponent<TestDummy>();
 
                 // If the Player is alive
-                if(d != null)
+                if (d != null)
                 {
                     Debug.Log("Dealing " + dmg + " damage to " + d);
                     d.takeDamage((int)dmg);
@@ -267,14 +187,5 @@ public class EnemyClass : MonoBehaviour
             player = null;
             state = State.Idle;
         }
-=======
-                    // UPDATE: if this is buggy try setting it to 1
-                wpIndex = 0;
-            }
-        }
-
-        // Calling to check if the Enemy is dead
-        Death();
->>>>>>> PlayerMovement
     }
 }
---
+++ Dasher/Assets/_Scripts/PlayerClass.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is the Player Class with Stats
+public class PlayerClass : MonoBehaviour
+{
+
+    // Basic Attributes
+    public float hp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int damage)
+    {
+        hp = hp - damage;
+
+        if (hp <= 0.0f)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        Debug.Log("Player has Died");
+        Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+}
